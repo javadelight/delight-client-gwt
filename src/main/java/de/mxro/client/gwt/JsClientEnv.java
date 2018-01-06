c66406223d2bc32f07b3312ff5807bee3d0013ff
Max Rohde <mxro@nowhere.com>
2018-01-06 12:06:21 +1100

    u

--- src/main/java/de/mxro/client/gwt/JsClientEnv.java
+++ src/main/java/de/mxro/client/gwt/JsClientEnv.java
@@ -17,30 +17,33 @@ import de.mxro.metrics.js.JsMetricsNode;
  * FOR CHRIS: No need to document the methods of this object.
  *
  */
-@jsinterop.annotations.JsType public class JsClientEnv  {
+@jsinterop.annotations.JsType
+public class JsClientEnv {
 
-    @jsinterop.annotations.JsIgnore
-    private ClientEnv wrapped;
+	@jsinterop.annotations.JsIgnore
+	private ClientEnv wrapped;
 
-    @delight.functional.annotations.ExportedElement public JsMetricsNode metrics() {
-        return JsMetricsNode.wrap(wrapped.metrics());
-    }
+	@delight.functional.annotations.ExportedElement
+	public JsMetricsNode metrics() {
+		return JsMetricsNode.wrap(wrapped.metrics());
+	}
 
-    @delight.functional.annotations.ExportedElement public JsLogsNode logs() {
-        return JsLogsNode.wrap(wrapped.logs());
-    }
+	@delight.functional.annotations.ExportedElement
+	public JsLogsNode logs() {
+		return JsLogsNode.wrap(wrapped.logs());
+	}
 
-    @jsinterop.annotations.JsIgnore
-    public static JsClientEnv wrap(final ClientEnv client) {
-        final JsClientEnv clientJs = new JsClientEnv();
+	@jsinterop.annotations.JsIgnore
+	public static JsClientEnv wrap(final ClientEnv client) {
+		final JsClientEnv clientJs = new JsClientEnv();
 
-        clientJs.wrapped = client;
+		clientJs.wrapped = client;
 
-        return clientJs;
-    }
+		return clientJs;
+	}
 
-    public JsClientEnv() {
-        super();
-    }
+	public JsClientEnv() {
+		super();
+	}
 
 }
